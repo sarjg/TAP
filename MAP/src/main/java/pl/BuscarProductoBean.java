544dd356d450fdd0362219f1c6a83c108c2d8b64
960076 <960076@vbox>
2023-04-24 16:33:25 +0200

    img terminada

--- MAP/src/main/java/pl/BuscarProductoBean.java
+++ MAP/src/main/java/pl/BuscarProductoBean.java
@@ -36,6 +36,7 @@ public class BuscarProductoBean implements Serializable {
     @PostConstruct
     public void init() {
 //    	cargarNombreSupermercado();
+    	
     }
 
     public void buscarProducto() {
